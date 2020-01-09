e0261ac3814c95b977638709d0734c98d0cc0b1c
Chris Luo <chris.ying.luo@gmail.com>
2020-01-09 15:40:12 -0800

    initial commit, added screens base

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import {
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import Colors from '../constants/Colors';
+import * as placesActions from '../store/places-actions';
+
+const NewPlaceScreen = props => {
+  const [titleValue, setTitleValue] = useState('');
+  const dispatch = useDispatch();
+
+  const titleChangeHandler = text => {
+    setTitleValue(text);
+  };
+
+  const savePlaceHandler = () => {
+    dispatch(placesActions.addPlace(titleValue));
+    props.navigation.goBack();
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          value={titleValue}
+          onChangeText={titleChangeHandler}
+        />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+NewPlaceScreen.navigationOptions = {
+  headerTitle: 'Add Place'
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
+
+export default NewPlaceScreen;
---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PlaceDetailScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>PlaceDetailScreen</Text>
+    </View>
+  );
+};
+
+PlaceDetailScreen.navigationOptions = navData => {
+  return {
+    headerTitle: navData.navigation.getParam('placeTitle')
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default PlaceDetailScreen;
